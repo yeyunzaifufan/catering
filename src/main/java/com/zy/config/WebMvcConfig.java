345447db01cfc64e263f8128aae695fde9708e30
Liu-Yang <18840830604@163.com>
2018-03-27 21:44:51 +0800

    2

--- src/main/java/com/zy/config/WebMvcConfig.java
+++ src/main/java/com/zy/config/WebMvcConfig.java
@@ -13,6 +13,21 @@ public class WebMvcConfig  extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/xx").setViewName("/xx");
+        registry.addViewController("/manager/top").setViewName("/manager/top");
+        registry.addViewController("/manager/left").setViewName("/manager/left");
+        registry.addViewController("/manager/index").setViewName("/manager/index");
+        registry.addViewController("/manager/main").setViewName("/manager/main");
+        registry.addViewController("/manager/computer").setViewName("/manager/computer");
+        registry.addViewController("/manager/default").setViewName("/manager/default");
+        registry.addViewController("/manager/error").setViewName("/manager/error");
+        registry.addViewController("/manager/filelist").setViewName("/manager/filelist");
+        registry.addViewController("/manager/imglist").setViewName("/manager/imglist");
+        registry.addViewController("/manager/imglist1").setViewName("/manager/imglist1");
+        registry.addViewController("/manager/imgtable").setViewName("/manager/imgtable");
+        registry.addViewController("/login").setViewName("/login");
+        registry.addViewController("/manager/right").setViewName("/manager/right");
+        registry.addViewController("/manager/tab").setViewName("/manager/tab");
+        registry.addViewController("/manager/tools").setViewName("/manager/tools");
     }
 
 }
